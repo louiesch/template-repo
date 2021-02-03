3f70428bdd07fc63928104fecf13d70716af40c4
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-03 10:46:54 -0800

    add all files for template

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// BUSINESS LOGIC ------------------
+
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
+// UI LOGIC ------------------------
